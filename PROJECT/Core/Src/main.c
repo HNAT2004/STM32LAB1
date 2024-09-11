9730fb328242e053bc276721474f79dc7f5ebd9b
Hồ Ngọc Anh Tuấn <tuan.hona2004@hcmut.edu.vn>
2024-09-11 21:48:57 +0700

    Init Ex9

--- PROJECT/Core/Src/main.c
+++ PROJECT/Core/Src/main.c
@@ -112,6 +112,49 @@ void setNumberOnClock(int num){
 		break;
 	}
 }
+
+void clearNumberOnClock(int num){
+	switch(num){
+		case 0:
+			HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, SET);
+			break;
+		case 1:
+			HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+			break;
+		case 2:
+			HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+			break;
+		case 3:
+			HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+			break;
+		case 4:
+			HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+			break;
+		case 5:
+			HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+			break;
+		case 6:
+			HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+			break;
+		case 7:
+			HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+			break;
+		case 8:
+			HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+			break;
+		case 9:
+			HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+			break;
+		case 10:
+			HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+			break;
+		case 11:
+			HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+			break;
+		default:
+			break;
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -151,12 +194,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  clearAllClock();
 	  HAL_Delay(1000);
-	  setNumberOnClock(1);
-	  HAL_Delay(1000);
-	  setNumberOnClock(6);
-	  HAL_Delay(1000);
+	  clearNumberOnClock(4);
+	  clearNumberOnClock(10);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
